28b2fdafbedab748537611f2c3c9610a36789e1d
sheng gao <goseng123@gmail.com>
2022-01-17 22:16:40 +0800

    scrap linkedin using sales navigator account

---
+++ LinkeIn_Scrap.py
@@ -0,0 +1,276 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import csv
+import os
+import openpyxl
+from tkinter import Tk
+import argparse
+from selenium.webdriver.common.keys import Keys
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+def scrap_from_url(args, driver,url=None, load_page=True):
+    if url == None:
+        url = 'https://www.linkedin.com/sales/search/people?doFetchHeroCard=false&functionIncluded=12&geoIncluded=103644278&industryIncluded=4&logHistory=true&page=1&rsLogId=967882860&searchSessionId=2KtadMyDTh6HsrXXvjHlIQ%3D%3D&seniorityIncluded=6%2C7%2C8'
+
+    if load_page:
+        driver.get(url)
+        time.sleep(3)
+#################################### INSERT URL ####################################
+
+    links = []
+    number_page = 1
+    number_candidate = 0
+    while number_page < 7:
+        time.sleep(3)
+        height = 0
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        while height < driver.execute_script("return document.body.scrollHeight"):
+            driver.execute_script("window.scrollTo(0, {});".format(height))
+            height += 20
+        names = driver.find_elements_by_tag_name("a")
+        for name in names:
+            link = name.get_attribute('href')
+            if 'https://www.linkedin.com/sales/people/' in link:
+                if link not in links:
+                    links.append(link)
+
+        number_page += 1
+        
+        next_button = driver.find_element_by_class_name("search-results__pagination-next-button")
+        next_button.click()
+        #print(links)
+        # print(len(links))
+
+    extracted_infos = []
+    hiring_keys = ['hiring','hiring!','Hiring','Hiring!','HIRING','open','Open','position']
+    count=0
+    for link in links:
+        count += 1
+        # if count > 1:
+        #     break
+        driver.get(link)
+        time.sleep(1)
+        person_profile = {}
+        ## Implement BeautifulSoup for html parser## TODO
+        src = driver.page_source
+        #print(src)
+        soup = BeautifulSoup(src, 'html.parser')
+
+        # get full name
+        full_name = soup.title.text.split('|')[0]
+
+        # get position and company name
+        pos_and_company = soup.find(class_='profile-topcard__current-positions flex mt3').text.split()
+        #print('**** {} ****'.format(pos_and_company))
+        index_at = pos_and_company.index('at')
+        position = ' '.join(pos_and_company[:index_at])
+        company = pos_and_company[index_at + 1]
+
+        # get connection size
+        try:
+            connection_size = soup.find(class_='profile-topcard__connections-data type-total inline t-14 t-black--light mr5').text.strip()
+        except:
+            connection_size = ''
+
+
+        # company url
+        try:
+            url_root = 'https://www.linkedin.com'
+            company_url = soup.find(class_='profile-topcard__current-positions flex mt3').a.attrs['href']
+            company_url = url_root + company_url
+        except:
+            company_url = ''
+
+        # linkedin profile url
+        try:
+            trip = driver.find_element_by_css_selector(
+                "div[class='artdeco-dropdown artdeco-dropdown--placement-bottom artdeco-dropdown--justification-right ember-view']")
+            trip.click()
+            cop_button = driver.find_element_by_css_selector("div[data-control-name='copy_linkedin']")
+            time.sleep(1)
+            cop_button.click()
+            time.sleep(1)
+            linkedin_url = Tk().clipboard_get()
+        except:
+            linkedin_url = ''
+
+        # profile description
+        try:
+            see_more_button = driver.find_element_by_css_selector(
+                "button[class='button--unstyled link-without-visited-state inline-block font-size-inherit profile-topcard__summary-expand-link']")
+            see_more_button.click()
+            src = driver.page_source
+            soup = BeautifulSoup(src, 'html.parser')
+            profile_summary = soup.find(class_='profile-topcard__summary-modal-content').text.strip()
+        except:
+            profile_summary = ''
+
+
+        # get hiring needs
+        hiring_needs = 'NO'
+        split_profile = profile_summary.split()
+        for word in split_profile:
+            if word in hiring_keys:
+                hiring_needs = 'YES'
+        top_card = soup.find(class_='profile-topcard-content-container mr2').text.strip().split()
+        for word in top_card:
+            if word in hiring_keys:
+                hiring_needs = 'YES'
+
+        #get previous position
+        
+        try:
+            pre_pos_and_company = soup.find(class_='profile-topcard__previous-positions flex mt3').text.split()
+        except:
+            pre_pos_and_company = []
+        ins = ''
+        for k in range(int(len(pre_pos_and_company)/2)):
+            ins += pre_pos_and_company[2*k] + ' ' + pre_pos_and_company[2*k+1] + ';'
+        pre_pos_and_company = ins
+        #print('**** {} ****'.format(pos_and_company))
+        #index_at = pos_and_company.index('at')
+        #position = ' '.join(pos_and_company[:index_at])
+        #company = pos_and_company[index_at + 1]
+        try:
+            edu1 = soup.find(class_='profile-education__school-name t-16 t-16--open t-bold t-black').text.split()
+        except:
+            edu1 = []
+        try:
+            edu2 = soup.find(class_='profile-education__degree inline t-14 t-black mb1').text.split()
+        except:
+            edu2 = []
+        try:
+            edu3 = soup.find(class_='profile-education__field-of-study inline t-14 t-black').text.split()
+        except:
+            edu3 = []
+        try:
+            edu4 = soup.find(class_='profile-education__dates t-12 t-black--light').text.split()
+        except:
+            edu4 = []
+        education = ' '.join(edu1)+';'+' '.join(edu2)+';'+' '.join(edu3)+';'+' '.join(edu4)
+        #education = ', '.join(education)
+        #print(education)
+        
+
+
+        ##---------------------------------------##
+        number_candidate += 1
+        print("{} /".format(number_candidate) + " {}".format(len(links)))
+        person_profile['NAME'] = full_name
+        person_profile['POSITION'] = position
+        person_profile['CONNECTION SIZE'] = connection_size
+        person_profile['COMPANY'] = company
+        person_profile['HIRING NEEDS'] = hiring_needs
+        person_profile['LINKEDIN WEBPAGE'] = linkedin_url
+        person_profile['COMPANY WEBPAGE'] = company_url
+        person_profile['SALE NAVIGATOR URL'] = link
+        person_profile['DESCRIPTION'] = profile_summary
+
+        person_profile['previous'] = pre_pos_and_company
+        person_profile['education'] = education
+        
+        extracted_infos.append(person_profile)
+        time.sleep(3)
+        #break
+
+    with open('results.csv', 'w', encoding='utf8', newline='') as output_file:
+        fc = csv.DictWriter(output_file, fieldnames=extracted_infos[0].keys())
+        fc.writeheader()
+        fc.writerows(extracted_infos)
+    # df = pd.DataFrame(np.array(tab))
+    # df.to_excel(r'{}/Linkedin_Scrap.xlsx'.format(path), index=False, header=True)
+    print('DONE')
+    return extracted_infos
+def scrap_from_keywords(args, driver,keywords=None):
+    #allcookies = driver.session_id#get_cookies()
+    #print(allcookies)
+    # filter_page_url = 'https://www.linkedin.com/sales/search/people?page=1&rsLogId=969614316&searchSessionId=1qpyQattToCh2lFvET1Cwg%3D%3D'
+    #filter_page_url = 'https://www.linkedin.com/sales/search/people?doFetchHeroCard=false&functionIncluded=12&geoIncluded=103644278&logHistory=true&page=1&rsLogId=994102108&searchSessionId=1qpyQattToCh2lFvET1Cwg%3D%3D&seniorityIncluded=6%2C8%2C7'
+    
+    filter_page_url = 'https://www.linkedin.com/sales/search/people?doFetchHeroCard=true&logHistory=true&rsLogId=1382593556&searchSessionId=1qpyQattToCh2lFvET1Cwg%3D%3D'
+    #p = filter_page_url.split('&')#&keywords=speech%20recognition
+    words = filter_page_url + '&keywords=' + '%20'.join(keywords.split(' '))
+    driver.get(words)
+    time.sleep(3)
+
+    #kwd_input = driver.find_element_by_id('bpr-guid-1612009') #017('ember44-input')
+    #kwd_input.send_keys(keywords)
+    #kwd_input.send_keys(Keys.ENTER)
+    time.sleep(1)
+    profiles = scrap_from_url(args, driver, url=driver.current_url, load_page=False)
+    return profiles
+def scrap_from_company_list(args, driver, fpath=''):
+    company_list = read_company_list(fpath)
+    rets = []
+    for company in company_list:
+        profiles = scrap_from_keywords(args, driver,keywords=company)
+        rets.extend(profiles)
+    with open('all_company.csv', 'w', encoding='utf8', newline='') as output_file:
+        fc = csv.DictWriter(output_file, fieldnames=rets[0].keys())
+        fc.writeheader()
+        fc.writerows(rets)
+    return rets
+def read_company_list(fpath):
+    with open(fpath) as f:
+        lines = f.readlines()
+    f.close()
+    lines = [line.strip() for line in lines]
+    return lines
+
+
+####################################################################################
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--search_words', type=str, help='search keywords')
+    parser.add_argument('--url', type=str, default=None)
+    parser.add_argument('--mode', type=str, default='url')
+    parser.add_argument('--email', type=str, help='sales natigator email')
+    parser.add_argument('--pwd', type=str, help='password')
+    args = parser.parse_args()
+
+
+    #driver = webdriver.Chrome()
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.get('https://www.linkedin.com')
+    driver.maximize_window()
+
+    username = driver.find_element_by_id('session_key')
+    username.send_keys(args.email)  # Insert your e-mail
+
+    password = driver.find_element_by_id('session_password')
+    password.send_keys(args.pwd)  # Insert your password here
+
+    log_in_button = driver.find_element_by_class_name("sign-in-form__submit-button")
+    log_in_button.click()
+    time.sleep(1)
+    try:
+        confirm_button = driver.find_element_by_id('remember-me-prompt__form-secondary')
+        confirm_button.click()
+    except:
+        pass
+    time.sleep(1)
+
+    print(args)
+
+
+    if args.mode == 'url':
+        scrap_from_url(args, driver, args.url)
+    elif args.mode == 'keywords':
+        scrap_from_keywords(args, driver, keywords=args.search_words)
+    elif args.mode == 'hugeworks':
+        scrap_from_company_list(args, driver, fpath='./company.txt')
+    else:
+        print('MODE wrongly specified!!!')
+
+
+
+
+
+
+
